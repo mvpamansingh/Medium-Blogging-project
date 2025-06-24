ce6783b4b111b64af0e289d9f87b9bc62d5cb752
mvpamansingh <amancodes5@gmail.com>
2025-06-25 02:06:31 +0530

    Implemented the CreateBlog page and design the skeleton for loading animation

---
+++ frontend/src/components/Skeleton.tsx
@@ -0,0 +1,20 @@
+
+function Skeleton()
+{
+    return(
+        <div>
+            <div role="status" className="max-w-sm animate-pulse mb-4">
+    <div className="h-2.5 bg-gray-200 rounded-full  w-48 mb-4"></div>
+    <div className="h-2 bg-gray-200 rounded-full  max-w-full mb-2.5"></div>
+    <div className="h-2 bg-gray-200 rounded-full  mb-2.5"></div>
+    <div className="h-2 bg-gray-200 rounded-full  max-w-[330px] mb-2.5"></div>
+    <div className="h-2 bg-gray-200 rounded-full  max-w-[300px] mb-2.5"></div>
+    <div className="h-2 bg-gray-200 rounded-full  max-w-[360px]"></div>
+    
+</div>
+        </div>
+    )
+}
+
+
+export {Skeleton};
